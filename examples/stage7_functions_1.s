4f7f17a78ac142404acd77cb0554f750a160c48c
geekSiddharth <siddharth16268@iiitd.ac.in>
2017-11-30 22:42:57 +0530

    mistakes ;)

---
+++ examples/stage7_functions_1.s
@@ -0,0 +1,71 @@
+	.arch armv7-a
+	.eabi_attribute 28, 1
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"stage7_functions_1.c"
+	.text
+	.align	1
+	.global	add
+	.syntax unified
+	.thumb
+	.thumb_func
+	.fpu vfpv3-d16
+	.type	add, %function
+add:
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	push	{r7}
+	sub	sp, sp, #20
+	add	r7, sp, #0
+	str	r0, [r7, #4]
+	str	r1, [r7]
+	ldr	r2, [r7, #4]
+	ldr	r3, [r7]
+	add	r3, r3, r2
+	str	r3, [r7, #12]
+	ldr	r3, [r7, #12]
+	mov	r0, r3
+	adds	r7, r7, #20
+	mov	sp, r7
+	@ sp needed
+	ldr	r7, [sp], #4
+	bx	lr
+	.size	add, .-add
+	.align	1
+	.global	main
+	.syntax unified
+	.thumb
+	.thumb_func
+	.fpu vfpv3-d16
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{r7, lr}
+	sub	sp, sp, #16
+	add	r7, sp, #0
+	movs	r3, #1
+	str	r3, [r7, #4]
+	movs	r3, #1
+	str	r3, [r7, #8]
+	ldr	r1, [r7, #8]
+	ldr	r0, [r7, #4]
+	bl	add(PLT)
+	str	r0, [r7, #12]
+	ldr	r3, [r7, #12]
+	mov	r0, r3
+	adds	r7, r7, #16
+	mov	sp, r7
+	@ sp needed
+	pop	{r7, pc}
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu/Linaro 7.2.0-6ubuntu1) 7.2.0"
+	.section	.note.GNU-stack,"",%progbits
