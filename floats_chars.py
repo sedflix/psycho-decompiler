b7512a22e27129a5e56e7a68fc2fe60257a8a42a
geekSiddharth <siddharth16268@iiitd.ac.in>
2017-11-30 21:51:06 +0530

    Comments added to Detecting Loop

--- floats_chars.py
+++ floats_chars.py
@@ -1,10 +1,4 @@
-from utill import *
 from DetectingLoops import *
-
-"""
-    Think about blocks and visibility 
-    association with r7,#4 and variables 
-"""
 
 
 class Function(object):
@@ -52,30 +46,71 @@ def getFunctions(linex):
         i += 1
     functions = []
     for i in range(len(lines.keys())):
-            if len(lines.keys())-i>2 and getOpcode(lines[i + 1]) == "push" and getOpcode(lines[i + 2]) == "sub":
-                x = getArgs(lines[i + 2])
-                j = i
-                for j in range(i, len(lines.keys())):
-                    if getOpcode(lines[j]) == "adds":
-                        y = getArgs(lines[j])
-                        if x[2] == y[2]:
-                            break
-                functions.append(Function(
-                    name=lines[i].replace(":", ""), start_line_text=lines[i + 1], start_line_no=i + 1,
-                    end_line_text=lines[j + 2], end_line_no=j + 2
-                ))
-            pass
+        """
+            Identifying the starts and ends of function
+        """
+        if len(lines.keys()) - i > 2 and getOpcode(lines[i + 1]) == "push" and getOpcode(lines[i + 2]) == "sub":
+            """
+                How to identify start of a function:
+                function detection can be done in the following ways:
+                "label" followed by a  "push" followed by  "sub, sp, sp #?" followed by "add r7, sp, 0"
+            """
+            x = getArgs(lines[i + 2])
+            j = i
+            for j in range(i, len(lines.keys())):
+                """
+                    Identify end of a loop
+
+                    end with adds r7, r7,  #? followed by mov sp, r7
+                    ###and maybe ldr r7, [sp], #4  followed by bx	lr
+                """
+                if getOpcode(lines[j]) == "adds":
+                    y = getArgs(lines[j])
+                    if x[2] == y[2]:
+                        """
+                            sp has to be subtracted in the beginning of the function and and added at the end of the function.
+                            This is our main idea to check the start and end of function
+                        """
+                        break
+            functions.append(Function(
+                name=lines[i].replace(":", ""), start_line_text=lines[i + 1],
+                start_line_no=i + 1, end_line_text=lines[j + 2], end_line_no=j + 2))
     for f in functions:
+        """
+                finding parameters of each function
+        """
         rhs = getRHS(lines, f)
         for i in range(f.start_line_no, f.end_line_no):
             opcode = getOpcode(lines[i])
+            """
+                ASSUMPTION: each argument of a function is first used with "str" or "mov" without initialisation of the same
+
+                int are usually stored with mov
+                while str for other type of data
+
+            """
             if "str" in opcode:
                 if getArgs(lines[i])[0] in rhs.keys():
+
+                    """
+                        This is too check if the  parameter/register 
+                        has been used before this line in function.
+                        z = a
+                        to filter "a" which hasn't been initialised before
+                    """
                     if rhs[getArgs(lines[i])[0]] >= i:
                         pass
                     else:
                         break
+
+                """
+                    To determine the parameter name
+                """
                 f.parameters.append(getArgs(lines[i])[0])
+
+                """
+                    Now we determine the type of parameters
+                """
                 if opcode == "vstr.32":
                     f.parameters_type.append("float")
                 elif opcode == "strb":
@@ -86,6 +121,13 @@ def getFunctions(linex):
                     f.parameters_type.append("int")
             elif "mov" in opcode:
                 if getArgs(lines[i])[0] in rhs.keys():
+                    """
+                        This is too check if the  parameter/register 
+                        has been used before this line in function.
+                        z = a
+                        to filter "a" which hasn't been initialised before
+                    """
+
                     if rhs[getArgs(lines[i])[0]] >= i:
                         pass
                     else:
@@ -93,21 +135,39 @@ def getFunctions(linex):
                 f.parameters.append(getArgs(lines[i])[1])
                 f.parameters_type.append("int")
     for i in range(len(lines.keys())):
+        """
+               Determine from where the function has been called.
+               Works only for "bl" 
+        """
         if "bl" in lines[i]:
             label = getArgs(lines[i])[0].split("(")[0]
             for f in functions:
                 if f.name == label:
                     f.callers_line_no.append(i)
     for f in functions:
+
+        """
+                finding return type of each function
+        """
+
         for i in range(f.end_line_no, f.start_line_no, -1):
             if len(f.return_) > 0 or isBranching(lines[i]):
                 break
             opcode = getOpcode(lines[i])
+
+            """
+                Algo/Assumption: return type of a function is last r0, s0 or d0 used with "str" or "mov"
+            """
+
             if "ldr" in opcode:
                 if not getArgs(lines[i])[0].endswith("0"):
                     continue
                 f.return_.append(getArgs(lines[i])[0])
                 f.return_line_no.append(i)
+
+                """
+                   Determine return time
+                """
                 if opcode == "vldr.32":
                     f.return_type.append("float")
                 elif opcode == "ldrb":
